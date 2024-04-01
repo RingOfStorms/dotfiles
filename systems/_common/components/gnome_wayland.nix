aa1d378a5bd65db15abcbd71347a290afb9cf856
ringofstorms <ringofstorms@gmail.com>
2024-04-01 11:18:07 -0500

    up to date

--- systems/_common/components/gnome_wayland.nix
+++ systems/_common/components/gnome_wayland.nix
@@ -1,12 +1,14 @@
 { pkgs, ... }:
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm = {
+  services.xserver = {
     enable = true;
-    autoSuspend = false;
-    wayland = true;
+    displayManager.gdm = {
+      enable = true;
+      autoSuspend = false;
+      wayland = true;
+    };
+    desktopManager.gnome.enable = true;
   };
-  services.xserver.desktopManager.gnome.enable = true;
   services.gnome.core-utilities.enable = false;
   environment.systemPackages = with pkgs; [
     gnome.dconf-editor
