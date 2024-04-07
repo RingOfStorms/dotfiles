5291ac3b47d0733820e0138f9ba774052f62c068
ringofstorms <ringofstorms@gmail.com>
2024-04-07 12:54:35 -0500

    per host nix modules by user

--- hosts/_common/components/home_manager.nix
+++ hosts/_common/components/home_manager.nix
@@ -3,7 +3,7 @@ let
   home-manager = builtins.fetchTarball {
     url = "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
     # to get hash run `nix-prefetch-url --unpack "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz"`
-    sha256 = "0g51f2hz13dk953i501fmc6935difhz60741nypaqwz127hy5ldk";
+    sha256 = "0r19x4n1wlsr9i3w4rlc4jc5azhv2yq1n3qb624p0dhhwfj3c3vl";
   };
 in
 {
@@ -18,5 +18,4 @@ in
   home-manager.useGlobalPkgs = true;
   home-manager.extraSpecialArgs = { inherit settings; inherit ylib; inherit (inputs) ragenix; inherit (config) age; };
 }
-
 
