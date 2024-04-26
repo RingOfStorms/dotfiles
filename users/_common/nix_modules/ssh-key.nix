160b567583166e1925617a7e35de6780c3e614c4
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2024-04-25 19:22:42 -0500

    gpdPocket3 updates

--- users/_common/nix_modules/ssh-key.nix
+++ users/_common/nix_modules/ssh-key.nix
@@ -1,21 +1,4 @@
 { settings, pkgs, ... }:
-let
-  sshScript = pkgs.writeScript "ssh-key-generation" ''
-    #!${pkgs.stdenv.shell}
-    if [ ! -f /home/${settings.user.username}/.ssh/id_ed25519]; then
-      if [ -v DRY_RUN ]; then
-        echo "DRY_RUN is set. Would generate SSH key for ${settings.user.username}."
-      else
-        echo "Generating SSH key for ${settings.user.username}."
-        mkdir -p /home/${settings.user.username}/.ssh
-        chmod 700 /home/${settings.user.username}/.ssh
-        /run/current-system/sw/bin/ssh-keygen -t ed25519 -f /home/${settings.user.username}/.ssh/id_ed25519-N ""
-      fi
-    else
-      echo "SSH key already exists for ${settings.user.username}."
-    fi
-  '';
-in
 {
   # Ensure SSH key pair generation for non-root users
   systemd.services.generate_ssh_key = {
@@ -24,7 +7,21 @@ in
     serviceConfig = {
       User = "${settings.user.username}";
       Type = "oneshot";
-      ExecStart = sshScript;
     };
+    script = ''
+      #!/run/current-system/sw/bin/bash
+      if [ ! -f /home/${settings.user.username}/.ssh/id_ed25519bbb ]; then
+        if [ -v DRY_RUN ]; then
+          echo "DRY_RUN is set. Would generate SSH key for ${settings.user.username}."
+        else
+          echo "Generating SSH key for ${settings.user.username}."
+          mkdir -p /home/${settings.user.username}/.ssh
+          chmod 700 /home/${settings.user.username}/.ssh
+          /run/current-system/sw/bin/ssh-keygen -t ed25519 -f /home/${settings.user.username}/.ssh/id_ed25519bbb -N ""
+        fi
+      else
+        echo "SSH key already exists for ${settings.user.username}."
+      fi
+    '';
   };
 }
