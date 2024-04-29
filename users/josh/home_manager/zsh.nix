bf63fb29cdc433999570bf443a81162f3cba1544
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2024-04-29 14:56:53 -0500

    add zsh behavior to the env instead of profile, I want it everywhere

--- users/josh/home_manager/zsh.nix
+++ users/josh/home_manager/zsh.nix
@@ -8,7 +8,7 @@
 
     defaultKeymap = "emacs";
 
-    profileExtra = ''
+    envExtra = ''
       # Make home/end and ctrl + left/right nav how I expect them to like in bash
       bindkey "\e[1~" beginning-of-line
       bindkey "\e[4~" end-of-line
