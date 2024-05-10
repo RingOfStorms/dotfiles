f9ec6f19ecbf25cb8f8067084cfb0d105812bd21
RingOfStorms (Josh) <ringofstorms@gmail.com>
2024-05-10 14:05:19 -0500

    add application launcher rofi

--- hosts/_common/components/docker.nix
+++ hosts/_common/components/docker.nix
@@ -2,4 +2,9 @@
 {
   virtualisation.docker.enable = true;
   users.extraGroups.docker.members = [ settings.user.username ];
+  environment.shellAliases = {
+    dockerv = "docker volume";
+    dockeri = "docker image";
+    dockerc = "docker container";
+  };
 }
---
+++ users/josh/home_manager/launcher.nix
@@ -0,0 +1,12 @@
+{ settings, pkgs, ... }:
+{
+  programs.rofi = {
+    enable = true;
+    plugins = with pkgs; [ rofi-calc ];
+    extraConfig = {
+      modi = "drun,run,ssh,window,calc";
+      terminal = "alacritty";
+    };
+    theme = "glue_pro_blue";
+  };
+}
