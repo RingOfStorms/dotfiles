2c15484586869082ddf15785e24e00e533374771
RingOfStorms (Josh) <ringofstorms@gmail.com>
2024-05-13 12:54:08 -0500

    fix users.user[s]

--- users/luser/by_hosts/nix_modules/ssh_authorized.nix
+++ users/luser/by_hosts/nix_modules/ssh_authorized.nix
@@ -1,6 +1,6 @@
 { settings, ... }:
 {
-  users.user.${settings.user.username}.openssh.authorizedKeys.keys = [
+  users.users.${settings.user.username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJie9OPheWn/EZWfXJSZ3S0DnISqI3ToCmOqhX/Tkwby nix2h002"
   ];
 }
