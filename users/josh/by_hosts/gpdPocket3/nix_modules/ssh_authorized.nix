4b33afcfa1fb5c1d308252bf10d880e4b1b8390f
RingOfStorms (Josh) <ringofstorms@gmail.com>
2024-05-13 12:54:08 -0500

    fix users.user[s]

--- users/josh/by_hosts/gpdPocket3/nix_modules/ssh_authorized.nix
+++ users/josh/by_hosts/gpdPocket3/nix_modules/ssh_authorized.nix
@@ -1,6 +1,6 @@
 { settings, ... }:
 {
-  users.user.${settings.user.username}.openssh.authorizedKeys.keys = [
+  users.users.${settings.user.username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDa0MUnXwRzHPTDakjzLTmye2GTFbRno+KVs0DSeIPb7 nix2gpdpocket3"
   ];
 }
--- users/josh/by_hosts/joe/nix_modules/ssh_authorized.nix
+++ users/josh/by_hosts/joe/nix_modules/ssh_authorized.nix
@@ -1,6 +1,6 @@
 { settings, ... }:
 {
-  users.user.${settings.user.username}.openssh.authorizedKeys.keys = [
+  users.users.${settings.user.username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINoBKfj+2SAlTxgdK1jYMFYoTTthX9jvfC+gko1Wlr4L nix2joe"
   ];
 }
