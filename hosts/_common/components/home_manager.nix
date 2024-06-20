01923f737aacb48f57ebdd4162ad7c4a75fa54d1
RingOfStorms (Josh) <ringofstorms@gmail.com>
2024-06-20 12:51:12 -0500

    separate out nixpkgs per host

--- hosts/_common/components/home_manager.nix
+++ hosts/_common/components/home_manager.nix
@@ -1,18 +1,23 @@
-{ config, pkgs, home-manager, settings, ylib, ... } @ inputs:
-# Note that we must have the channel added for the import to work below
-# `sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager`
-# `sudo nix-channel --update`
 {
-  imports =
-    [
-      # home manager import
-      home-manager.nixosModules.home-manager
-      # home-manager
-    ];
+  config,
+  settings,
+  ylib,
+  ...
+}@inputs:
+let
+  home-manager = settings.home-manager;
+in
+{
+  imports = [ home-manager.nixosModules.home-manager ];
+
   # Home manager options
   security.polkit.enable = true;
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
-  home-manager.extraSpecialArgs = { inherit settings; inherit ylib; inherit (inputs) ragenix; inherit (config) age; };
+  home-manager.extraSpecialArgs = {
+    inherit settings;
+    inherit ylib;
+    inherit (inputs) ragenix;
+    inherit (config) age;
+  };
 }
-
