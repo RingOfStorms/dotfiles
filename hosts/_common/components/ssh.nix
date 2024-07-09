72af92c4ebdeac884e6aece4c4dbfb0746d2be60
RingOfStorms (Josh) <ringofstorms@gmail.com>
2024-07-09 17:32:02 -0500

    add fail2ban on ssh

--- hosts/_common/components/ssh.nix
+++ hosts/_common/components/ssh.nix
@@ -1,10 +1,19 @@
 { ... }:
 {
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.settings.PermitRootLogin = "yes";
+  # Use fail2ban
+  services.fail2ban = {
+    enable = true;
+  };
+
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [
     22 # sshd
   ];
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    logLevel = "VERBOSE";
+    settings.PermitRootLogin = "yes";
+  };
 }
