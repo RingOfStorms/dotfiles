340d78d2873dc7d541337f604bb5d3c0e836a156
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-01-15 18:20:24 -0600

    updates

--- components/hm/alacritty.nix
+++ components/hm/alacritty.nix
@@ -1,11 +1,9 @@
 { ... }:
 {
-  # More of an experiment to try out since wezterm is being weird on wayland...
   programs.alacritty = {
     enable = true;
     settings = {
       window = {
-        # TODO revisit, I still want some border shadow but no top bar but that is not an option
         decorations = "None";
         dynamic_title = false;
       };
@@ -29,20 +27,7 @@
       font = {
         normal = { family = "JetBrainsMonoNL Nerd Font"; style = "Regular"; };
         size = 12.0;
-        ## TODO use 16 on macos ...
       };
-      # TODO revisit... none of this is working.
-      # NOTE: I probably wont need these anymore, I've since entirely remade and relearned my tmux shortcuts to not be based on these
-      keyboard.bindings = [
-        # { key = "m"; mods = "Command"; chars = "test"; }
-        # { key = "t"; mods = "Control"; action = { SendString = "\\x01t"; }; }
-        # { key = "w"; mods = "Control"; action = { SendString = "\\x01w"; }; }
-        # { key = "o"; mods = "Control"; action = { SendString = "testing123"; }; }
-        # { key = "w"; mods = "Control"; chars = "\\\\x01w"; }
-        # { key = "o"; mods = "Control"; chars = "\\\\x01o"; }
-        # { key = "1"; mods = "Control"; chars = "\\\\x011"; }
-        # { key = "2"; mods = "Control"; chars = "\\\\x012"; }
-      ];
     };
   };
 }
