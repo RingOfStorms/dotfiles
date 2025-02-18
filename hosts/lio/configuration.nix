19f6918d66d09ec26a755f8fca9bf386c324e811
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-02-18 00:57:25 -0600

    updates

--- hosts/lio/configuration.nix
+++ hosts/lio/configuration.nix
@@ -19,6 +19,9 @@
 
   # System76
   hardware.system76.enableAll = true;
+  # https://discourse.nixos.org/t/very-high-fan-noises-on-nixos-using-a-system76-thelio/23875/10
+  # Fixes insane jet speed fan noise
+  services.power-profiles-daemon.enable = false;
 
   system.stateVersion = "23.11";
 }
