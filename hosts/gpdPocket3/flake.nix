caf06a336b69c02c761015bffa3aa4e2af5b31f4
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-03-18 14:41:30 -0500

    update gp3

--- hosts/gpdPocket3/flake.nix
+++ hosts/gpdPocket3/flake.nix
@@ -2,8 +2,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    common.url = "path:../../common";
-    # common.url = "git+https://git.joshuabell.xyz/dotfiles?dir=common";
+    # for local testing.
+    # common.url = "path:../../common
+    common.url = "git+https://git.joshuabell.xyz/dotfiles";
 
     ros_neovim.url = "git+https://git.joshuabell.xyz/nvim";
   };
@@ -12,27 +13,20 @@
     {
       nixpkgs,
       common,
+      ros_neovim,
       ...
-    }@inputs:
+    }:
     let
       configuration_name = "gpdPocket3";
       lib = nixpkgs.lib;
     in
     {
       nixosConfigurations = {
-        "${configuration_name}" =
-          let
-            auto_modules = builtins.concatMap (
-              input:
-              lib.optionals
-                (builtins.hasAttr "nixosModules" input && builtins.hasAttr "default" input.nixosModules)
-                [
-                  input.nixosModules.default
-                ]
-            ) (builtins.attrValues inputs);
-          in
-          (lib.nixosSystem {
+        "${configuration_name}" = (
+          lib.nixosSystem {
             modules = [
+              common.nixosModules.default
+              ros_neovim.nixosModules.default
               ./configuration.nix
               ./hardware-configuration.nix
               (
@@ -94,11 +88,9 @@
                   };
                 }
               )
-            ] ++ auto_modules;
-            specialArgs = {
-              inherit inputs;
-            };
-          });
+            ];
+          }
+        );
       };
     };
 }
