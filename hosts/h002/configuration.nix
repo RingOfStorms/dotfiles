9ecc051a3b0133728f117a196f042e3bfbaa0dd9
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-03-19 17:14:14 -0500

    I give up I cant get wifi card to work

--- hosts/h002/configuration.nix
+++ hosts/h002/configuration.nix
@@ -1,19 +1,39 @@
 {
   pkgs,
+  config,
   ...
 }:
 {
   # machine specific configuration
   # ==============================
   hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
   # Connectivity
   networking.networkmanager.enable = true;
   hardware.bluetooth.enable = true;
+  # networking.networkmanager.unmanaged = [ "interface-name:wlp*" ]; # Mark wireless as unmanaged
   environment.shellAliases = {
     wifi = "nmtui";
   };
 
-  environment.systemPackages = with pkgs; [ nvtopPackages.full ];
+  # Realtek wireless module support
+  # Ensure the rtl8192 module is loaded (RTL8190 typically uses rtl8192 driver)
+
+  # boot.extraModulePackages = [ config.boot.kernelPackages.rtl8192eu ];
+  boot.kernelModules = [
+    "rtl8192ce"
+    "rtl8192c_common"
+    "rtlwifi"
+    "mac80211"
+  ];
+  # Install network management tools
+  environment.systemPackages = with pkgs; [
+    pciutils
+    wirelesstools
+    iw
+    networkmanager
+    nvtopPackages.full
+  ];
 
   system.stateVersion = "23.11";
 }
