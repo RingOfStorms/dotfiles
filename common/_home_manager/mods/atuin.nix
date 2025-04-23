c2845c31497a93af8fb7f681cdf8fd3db063df82
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-04-23 16:37:18 -0500

    update atuin server

--- common/_home_manager/mods/atuin.nix
+++ common/_home_manager/mods/atuin.nix
@@ -8,7 +8,7 @@
       workspaces = true;
       exit-mode = "return-query";
       enter_accept = true;
-      sync_address = "https://atuin.joshua.xyz";
+      sync_address = "https://atuin.joshuabell.xyz";
       sync = { records = true; };
     };
   };
