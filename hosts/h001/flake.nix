52f56866ec2c51623ddbe1e2759137dc2b79fb44
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-04-30 14:53:00 -0500

    enable adguard home

--- hosts/h001/flake.nix
+++ hosts/h001/flake.nix
@@ -37,6 +37,7 @@
                 {
                   environment.systemPackages = with pkgs; [
                     lua
+                    sqlite
                   ];
 
                   ringofstorms_common = {
--- hosts/h001/mods/adguardhome.nix
+++ hosts/h001/mods/adguardhome.nix
@@ -6,7 +6,33 @@
     services.adguardhome = {
       enable = true;
       allowDHCP = true;
-      openFirewall = true;
+      openFirewall = false;
+    };
+
+    networking.firewall.allowedTCPPorts = [
+      53
+      67
+      68
+      5543
+    ];
+    networking.firewall.allowedUDPPorts = [
+      53
+      67
+      68
+      784
+      853
+      8853
+      5443
+    ];
+
+    services.nginx = {
+      virtualHosts = {
+        "h001.net.joshuabell.xyz  " = {
+          locations."/" = {
+            proxyPass = "http://localhost:3000";
+          };
+        };
+      };
     };
   };
 }
