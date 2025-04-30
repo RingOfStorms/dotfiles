500549f7c938829cbd54ca5515583303541409e8
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-04-30 17:31:05 -0500

    not sure on dhcp server in adguard home reverting back to router for now

--- hosts/h001/mods/adguardhome.nix
+++ hosts/h001/mods/adguardhome.nix
@@ -25,6 +25,20 @@
       5443
     ];
 
+    # networking = {
+    #   interfaces = {
+    #     enp0s31f6 = {
+    #       useDHCP = true;
+    #       ipv4.addresses = [
+    #         {
+    #           address = "10.12.14.2";
+    #           prefixLength = 24;
+    #         }
+    #       ];
+    #     };
+    #   };
+    # };
+
     services.nginx = {
       virtualHosts = {
         "h001.net.joshuabell.xyz  " = {
