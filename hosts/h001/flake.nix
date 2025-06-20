e4abaea043db6b57576437f2422682af56f3a44d
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-06-20 16:31:03 -0500

    update to 25.05 for home server

--- hosts/h001/flake.nix
+++ hosts/h001/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Use relative to get current version for testing
