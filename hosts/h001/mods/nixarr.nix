8f3836aa0090f102d54272af540e2d94534ad11e
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-06-20 22:43:46 -0500

    update h001 for media

--- hosts/h001/mods/nixarr.nix
+++ hosts/h001/mods/nixarr.nix
@@ -5,10 +5,11 @@
   config = {
     nixarr = {
       enable = true;
-      mediaDir = "/var/lib/nixarr/media";
+      mediaDir = "/drives/wd10/nixarr/media";
       stateDir = "/var/lib/nixarr/state";
 
       jellyfin.enable = true; # jellyfinnnnnn!
+      jellyseerr.enable = true; # request manager for media
       sabnzbd.enable = true; # Usenet downloader
       prowlarr.enable = true; # Index manager
       sonarr.enable = true; # TV
@@ -16,7 +17,81 @@
       bazarr.enable = true; # subtitles for sonarr and radarr
       lidarr.enable = true; # music
       readarr.enable = true; # books
-      jellyseerr.enable = true; # request manager for media
+    };
+
+    services.nginx = {
+      virtualHosts = {
+        "jellyfin.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:8096";
+          };
+        };
+        "media.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:5055";
+          };
+        };
+        "100.64.0.13" = {
+          locations = {
+            "/jellyfin" = {
+              proxyPass = "http://localhost:8096";
+              extraConfig = ''
+                rewrite ^/jellyfin/(.*) /$1 break;
+              '';
+            };
+            "/jellyseerr" = {
+              proxyPass = "http://localhost:5055";
+              extraConfig = ''
+                rewrite ^/jellyseerr/(.*) /$1 break;
+              '';
+            };
+            "/sabnzbd" = {
+              proxyPass = "http://localhost:6336";
+              extraConfig = ''
+                rewrite ^/sabnzbd/(.*) /$1 break;
+              '';
+            };
+            "/prowlarr" = {
+              proxyPass = "http://localhost:9696";
+              extraConfig = ''
+                rewrite ^/prowlarr/(.*) /$1 break;
+              '';
+            };
+            "/radarr" = {
+              proxyPass = "http://localhost:7878";
+              extraConfig = ''
+                rewrite ^/radarr/(.*) /$1 break;
+              '';
+            };
+            "/sonarr" = {
+              proxyPass = "http://localhost:8989";
+              extraConfig = ''
+                rewrite ^/sonarr/(.*) /$1 break;
+              '';
+            };
+            "/lidarr" = {
+              proxyPass = "http://localhost:8686";
+              extraConfig = ''
+                rewrite ^/lidarr/(.*) /$1 break;
+              '';
+            };
+            "/readarr" = {
+              proxyPass = "http://localhost:8787";
+              extraConfig = ''
+                rewrite ^/readarr/(.*) /$1 break;
+              '';
+            };
+            "/bazarr" = {
+              proxyPass = "http://localhost:6767";
+              extraConfig = ''
+                rewrite ^/bazarr/(.*) /$1 break;
+              '';
+            };
+          };
+        };
+      };
     };
   };
 }
