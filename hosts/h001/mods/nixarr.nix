340f3e3ff4062037e917ad5cbd98b336f265e22d
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-06-23 10:28:24 -0500

    wip nixarr

--- hosts/h001/mods/nixarr.nix
+++ hosts/h001/mods/nixarr.nix
@@ -33,62 +33,70 @@
             proxyPass = "http://localhost:5055";
           };
         };
-        "100.64.0.13" = {
-          locations = {
-            "/jellyfin" = {
-              proxyPass = "http://localhost:8096";
-              extraConfig = ''
-                rewrite ^/jellyfin/(.*) /$1 break;
-              '';
-            };
-            "/jellyseerr" = {
-              proxyPass = "http://localhost:5055";
-              extraConfig = ''
-                rewrite ^/jellyseerr/(.*) /$1 break;
-              '';
-            };
-            "/sabnzbd" = {
-              proxyPass = "http://localhost:6336";
-              extraConfig = ''
-                rewrite ^/sabnzbd/(.*) /$1 break;
-              '';
-            };
-            "/prowlarr" = {
-              proxyPass = "http://localhost:9696";
-              extraConfig = ''
-                rewrite ^/prowlarr/(.*) /$1 break;
-              '';
-            };
-            "/radarr" = {
-              proxyPass = "http://localhost:7878";
-              extraConfig = ''
-                rewrite ^/radarr/(.*) /$1 break;
-              '';
-            };
-            "/sonarr" = {
-              proxyPass = "http://localhost:8989";
-              extraConfig = ''
-                rewrite ^/sonarr/(.*) /$1 break;
-              '';
-            };
-            "/lidarr" = {
-              proxyPass = "http://localhost:8686";
-              extraConfig = ''
-                rewrite ^/lidarr/(.*) /$1 break;
-              '';
-            };
-            "/readarr" = {
-              proxyPass = "http://localhost:8787";
-              extraConfig = ''
-                rewrite ^/readarr/(.*) /$1 break;
-              '';
-            };
-            "/bazarr" = {
-              proxyPass = "http://localhost:6767";
-              extraConfig = ''
-                rewrite ^/bazarr/(.*) /$1 break;
-              '';
-            };
+        "jellyfin.h001.local.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:8096";
+          };
+        };
+        "media.h001.local.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:5055";
+          };
+        };
+        "jellyfin.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:8096";
+          };
+        };
+        "media.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:5055";
+          };
+        };
+        "sabnzbd.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:6336";
+          };
+        };
+        "prowlarr.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:9696";
+          };
+        };
+        "radarr.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:7878";
+          };
+        };
+        "sonarr.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:8989";
+          };
+        };
+        "lidarr.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:8686";
+          };
+        };
+        "readarr.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:8787";
+          };
+        };
+        "bazarr.h001.n.joshuabell.xyz" = {
+          locations."/" = {
+            proxyWebsockets = true;
+            proxyPass = "http://localhost:6767";
           };
         };
       };
