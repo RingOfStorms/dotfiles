6c0c9f3ff4ffaaebd605db6f66a63d8feaebce3b
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-07-23 13:16:11 -0500

    update lio h001

--- hosts/h001/mods/nixarr.nix
+++ hosts/h001/mods/nixarr.nix
@@ -1,4 +1,5 @@
 {
+  config,
   ...
 }:
 {
@@ -8,9 +9,21 @@
       mediaDir = "/drives/wd10/nixarr/media";
       stateDir = "/var/lib/nixarr/state";
 
+      vpn = {
+        enable = true;
+        wgConf = config.age.secrets.us_chi_wg.path;
+      };
+
       jellyfin.enable = true; # jellyfinnnnnn!
+      jellyfin.vpn.enable = true;
       jellyseerr.enable = true; # request manager for media
+      jellyseerr.vpn.enable = true;
       sabnzbd.enable = true; # Usenet downloader
+      transmission = {
+        enable = true; # Torrent downloader
+        vpn.enable = true;
+        peerPort = 51820;
+      };
       prowlarr.enable = true; # Index manager
       sonarr.enable = true; # TV
       radarr.enable = true; # Movies
