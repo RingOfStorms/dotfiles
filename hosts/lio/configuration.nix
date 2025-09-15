5e23c395d7172132e87c2994a23aa4ea0fa0c24e
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-09-15 00:15:08 -0500

    idk power stuff

--- hosts/lio/configuration.nix
+++ hosts/lio/configuration.nix
@@ -10,9 +10,22 @@
 
   # System76
   hardware.system76.enableAll = true;
-  # https://discourse.nixos.org/t/very-high-fan-noises-on-nixos-using-a-system76-thelio/23875/10
-  # Fixes insane jet speed fan noise
-  services.power-profiles-daemon.enable = false;
 
   system.stateVersion = "23.11";
+
+  services = {
+    # https://discourse.nixos.org/t/very-high-fan-noises-on-nixos-using-a-system76-thelio/23875/10
+    # Fixes insane jet speed fan noise
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      # settings = {
+      #   CPU_BOOST_ON_AC = 1;
+      #   CPU_BOOST_ON_BAT = 0;
+      #   CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      #   CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      #   STOP_CHARGE_THRESH_BAT0 = 95;
+      # };
+    };
+  };
 }
