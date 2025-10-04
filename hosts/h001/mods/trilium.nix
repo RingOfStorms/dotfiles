84202da924ecaf7e3810e9c4ef9cb442a07a5916
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-10-04 00:26:05 -0500

    will this work?

--- hosts/h001/mods/trilium.nix
+++ hosts/h001/mods/trilium.nix
@@ -23,6 +23,22 @@
           proxyWebsockets = true;
           recommendedProxySettings = true;
           proxyPass = "http://127.0.0.1:9111";
+          extraConfig = ''
+            auth_request /oauth2/auth;
+            error_page 401 = @error401;
+
+            location = /oauth2/auth {
+              internal;
+              proxy_pass https://sso-proxy.joshuabell.xyz/oauth2/auth;
+              proxy_set_header Host sso-proxy.joshuabell.xyz;
+              proxy_set_header X-Auth-Request-Redirect $request_uri;
+              proxy_set_header X-Forwarded-Proto https;
+            }
+
+            location @error401 {
+              return 302 https://sso-proxy.joshuabell.xyz/oauth2/start?rd=https://$host$request_uri;
+            }
+          '';
         };
       };
     };
