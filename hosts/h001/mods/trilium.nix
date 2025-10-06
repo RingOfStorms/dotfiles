3816844590c5bb5c289d27fe4e073bf3d08f7b82
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-10-05 23:06:34 -0500

    add a tailnet overlay network verison for trillium to test the android app

--- hosts/h001/mods/trilium.nix
+++ hosts/h001/mods/trilium.nix
@@ -1,10 +1,12 @@
 {
+  upkgs,
   ...
 }:
 {
   config = {
     services.trilium-server = {
       enable = true;
+      package = upkgs.trilium-server;
       port = 9111;
       host = "127.0.0.1";
       dataDir = "/var/lib/trilium";
@@ -19,12 +21,30 @@
     services.oauth2-proxy.nginx.virtualHosts."notes.joshuabell.xyz" = {
       allowed_groups = [ "notes" ];
     };
-    services.nginx.virtualHosts."notes.joshuabell.xyz" = {
-      locations = {
-        "/" = {
-          proxyWebsockets = true;
-          recommendedProxySettings = true;
-          proxyPass = "http://127.0.0.1:9111";
+    services.nginx.virtualHosts = {
+      "notes.joshuabell.xyz" = {
+        locations = {
+          "/" = {
+            proxyWebsockets = true;
+            recommendedProxySettings = true;
+            proxyPass = "http://127.0.0.1:9111";
+          };
+        };
+      };
+      "trilium_overlay" = {
+        serverName = "h001.net.joshuabell.xyz";
+        listen = [
+          {
+            port = 9112;
+            addr = "100.64.0.13";
+          }
+        ];
+        locations = {
+          "/" = {
+            proxyWebsockets = true;
+            recommendedProxySettings = true;
+            proxyPass = "http://127.0.0.1:9111";
+          };
         };
       };
     };
