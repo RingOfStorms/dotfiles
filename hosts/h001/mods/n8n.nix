528bea5d733ae000ad2cbfe2a7ff0f3a975b0a8b
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-10-08 14:20:50 -0500

    change n8n to be behind reverse proxy

--- hosts/h001/mods/n8n.nix
+++ hosts/h001/mods/n8n.nix
@@ -1,9 +1,6 @@
 {
   ...
 }:
-let
-  port = 5678;
-in
 {
   options = { };
   config = {
@@ -13,21 +10,27 @@ in
           "/" = {
             proxyWebsockets = true;
             recommendedProxySettings = true;
-            proxyPass = "http://127.0.0.1:${port}";
+            proxyPass = "http://127.0.0.1:5678";
           };
         };
       };
     };
 
     # Expose litellm to my overlay network as well
-    networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ port ];
+    # networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ port ];
 
     services.n8n = {
       enable = true;
     };
     systemd.services.n8n.environment = {
       # N8N_SECURE_COOKIE = "false";
+      N8N_LISTEN_ADDRESS = "127.0.0.1";
       N8N_EDITOR_BASE_URL = "https://n8n.joshuabell.xyz/";
+      N8N_HOST = "n8n.joshuabell.xyz";
+      VUE_APP_URL_BASE_API = "https://n8n.joshuabell.xyz/";
+      N8N_HIRING_BANNER_ENABLED = "false";
+      N8N_PUBLIC_API_DISABLED = "true";
+      N8N_PUBLIC_API_SWAGGERUI_DISABLED = "true";
     };
   };
 }
