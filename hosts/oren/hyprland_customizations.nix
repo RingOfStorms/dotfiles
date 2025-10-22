c50403f5dc4e6ef720959b47e8c613852bdb8c49
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-10-22 00:19:20 -0500

    oren on new version success

--- hosts/oren/hyprland_customizations.nix
+++ hosts/oren/hyprland_customizations.nix
@@ -1,21 +1,18 @@
-{ ... }:
-let
-  hyprlandExtraOptions = {
-    exec-once = [
-      # Wait a moment for monitors/workspaces to settle, then "prime" 6 and return to 1
-      "sh -lc 'sleep 0.2; hyprctl dispatch workspace 1'"
-    ];
-    monitor = [
-      "eDP-1,2560x1600@165.000Hz,0x0,1.666667,transform,0"
-    ];
-  };
-in
+{ lib, ... }:
 {
   home-manager.sharedModules = [
     (
       { ... }:
       {
-        wayland.windowManager.hyprland.settings = hyprlandExtraOptions;
+        wayland.windowManager.hyprland.settings = {
+          exec-once = [
+            # Wait a moment for monitors/workspaces to settle, then "prime" 6 and return to 1
+            "sh -lc 'sleep 0.2; hyprctl dispatch workspace 1'"
+          ];
+          monitor = lib.mkForce [
+            "eDP-1,2560x1600@165.000Hz,0x0,1.666667,transform,0"
+          ];
+        };
         services.hyprpaper.settings = {
           mode = "fill"; # Wallpaper display mode: fill, fit, stretch, center, tile
 
