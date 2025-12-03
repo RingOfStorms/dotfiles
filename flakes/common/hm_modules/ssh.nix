10cb42ba2109fd52c787efa1e8fc6dffab90e169
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-12-03 11:36:39 -0600

    fix 25.11 deprecations

--- flakes/common/hm_modules/ssh.nix
+++ flakes/common/hm_modules/ssh.nix
@@ -20,7 +20,21 @@ in
       Host *
         SetEnv TERM=xterm-256color
     '';
+    enableDefaultConfig = false;
     matchBlocks = {
+      "*" = {
+        forwardAgent = false;
+        addKeysToAgent = "no";
+        compression = false;
+        serverAliveInterval = 0;
+        serverAliveCountMax = 3;
+        hashKnownHosts = false;
+        userKnownHostsFile = "~/.ssh/known_hosts";
+        controlMaster = "no";
+        controlPath = "~/.ssh/master-%r@%n:%p";
+        controlPersist = "no";
+      };
+
       # EXTERNAL
       "github.com" = lib.mkIf (hasSecret "nix2github") {
         identityFile = age.secrets.nix2github.path;
