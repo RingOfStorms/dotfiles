a27c00c6ff082321526e3809a60ce3ead2abee95
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-12-29 16:46:30 -0600

    prepare new host juni

---
+++ hosts/juni/flake.nix
@@ -0,0 +1,166 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager.url = "github:rycee/home-manager/release-25.11";
+
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # Use relative to get current version for testin
+    # inputs.common.url = "path:../../flakes/common";
+    inputs.common.url = "git+https://git.joshuabell.xyz/ringofstorms/dotfiles?dir=flakes/common";
+    # secrets.url = "path:../../flakes/secrets";
+    secrets.url = "git+https://git.joshuabell.xyz/ringofstorms/dotfiles?dir=flakes/secrets";
+    # flatpaks.url = "path:../../flakes/flatpaks";
+    flatpaks.url = "git+https://git.joshuabell.xyz/ringofstorms/dotfiles?dir=flakes/flatpaks";
+    # beszel.url = "path:../../flakes/beszel";
+    beszel.url = "git+https://git.joshuabell.xyz/ringofstorms/dotfiles?dir=flakes/beszel";
+    # de_plasma.url = "path:../../flakes/de_plasma";
+    de_plasma.url = "git+https://git.joshuabell.xyz/ringofstorms/dotfiles?dir=flakes/de_plasma";
+    # opencode.url = "path:../../flakes/opencode";
+    opencode.url = "git+https://git.joshuabell.xyz/ringofstorms/dotfiles?dir=flakes/opencode";
+
+    ros_neovim.url = "git+https://git.joshuabell.xyz/ringofstorms/nvim";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      nixpkgs-unstable,
+      ...
+    }@inputs:
+    let
+      configuration_name = "juni";
+      stateVersion = "25.11";
+      primaryUser = "josh";
+      lib = nixpkgs.lib;
+    in
+    {
+      nixosConfigurations = {
+        "${configuration_name}" = (
+          lib.nixosSystem {
+            modules = [
+              ({
+                nixpkgs.overlays = [
+                  (final: prev: {
+                    unstable = import nixpkgs-unstable {
+                      inherit (final) system config;
+                    };
+                  })
+                ];
+              })
+              home-manager.nixosModules.default
+
+              inputs.de_plasma.nixosModules.default
+              ({
+                ringofstorms.dePlasma = {
+                  enable = true;
+                  gpu.amd.enable = true;
+                  sddm.autologinUser = "josh";
+                };
+              })
+
+              # secrets.nixosModules.default
+              inputs.ros_neovim.nixosModules.default
+              ({
+                ringofstorms-nvim.includeAllRuntimeDependencies = true;
+              })
+              # inputs.opencode.nixosModules.default
+
+              inputs.flatpaks.nixosModules.default
+
+              inputs.common.nixosModules.essentials
+              inputs.common.nixosModules.git
+              inputs.common.nixosModules.tmux
+              inputs.common.nixosModules.boot_systemd
+              inputs.common.nixosModules.hardening
+              inputs.common.nixosModules.jetbrains_font
+              inputs.common.nixosModules.nix_options
+              # inputs.common.nixosModules.tailnet
+              inputs.common.nixosModules.timezone_auto
+              inputs.common.nixosModules.tty_caps_esc
+              inputs.common.nixosModules.zsh
+
+              # beszel.nixosModules.agent
+              # ({
+              #     beszelAgent = {
+              #       token = "f8a54c41-486b-487a-a78d-a087385c317b";
+              #     };
+              #   }
+              # )
+
+              ./configuration.nix
+              ./hardware-configuration.nix
+              (
+                { config, pkgs, ... }:
+                rec {
+                  # Home Manager
+                  home-manager = {
+                    useUserPackages = true;
+                    useGlobalPkgs = true;
+                    backupFileExtension = "bak";
+                    # add all normal users to home manager so it applies to them
+                    users = lib.mapAttrs (name: user: {
+                      home.stateVersion = stateVersion;
+                      programs.home-manager.enable = true;
+                    }) (lib.filterAttrs (name: user: user.isNormalUser or false) users.users);
+
+                    sharedModules = [
+                      inputs.common.homeManagerModules.tmux
+                      inputs.common.homeManagerModules.atuin
+                      inputs.common.homeManagerModules.direnv
+                      inputs.common.homeManagerModules.kitty
+                      inputs.common.homeManagerModules.git
+                      inputs.common.homeManagerModules.postgres_cli_options
+                      inputs.common.homeManagerModules.ssh
+                      inputs.common.homeManagerModules.starship
+                      inputs.common.homeManagerModules.zoxide
+                      inputs.common.homeManagerModules.zsh
+                      (
+                        { ... }:
+                        {
+                          programs.tmux.package = pkgs.unstable.tmux;
+                        }
+                      )
+                    ];
+                  };
+
+                  # System configuration
+                  system.stateVersion = stateVersion;
+                  networking.hostName = configuration_name;
+                  programs.nh.flake = "/home/${primaryUser}/.config/nixos-config/hosts/${config.networking.hostName}";
+                  nixpkgs.config.allowUnfree = true;
+                  users.users = {
+                    "${primaryUser}" = {
+                      isNormalUser = true;
+                      initialPassword = "password1";
+                      extraGroups = [
+                        "wheel"
+                        "networkmanager"
+                        "video"
+                        "input"
+                      ];
+                      openssh.authorizedKeys.keys = [
+                        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH2KFSRkViT+asBTjCgA7LNP3SHnfNCW+jHbV08VUuIi nix2nix"
+                      ];
+                    };
+                  };
+
+                  environment.systemPackages = with pkgs; [
+                    vlc
+                    google-chrome
+                  ];
+
+                  services.flatpak.packages = [
+                    "dev.vencord.Vesktop"
+                    "com.spotify.Client"
+                    "com.bitwarden.desktop"
+                  ];
+                }
+              )
+            ];
+          }
+        );
+      };
+    };
+}
