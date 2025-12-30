e3461aba5cba74632d9208d31ac821160c1157a4
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2025-12-30 00:16:13 -0600

    try some more hardware settings to get wifi working

--- hosts/juni/hardware-configuration.nix
+++ hosts/juni/hardware-configuration.nix
@@ -22,4 +22,10 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.enableAllFirmware = true;
+  hardware.bluetooth.enable = true;
+  hardware.enableRedistributableFirmware = true;
+  hardware.inputmodule.enable = true;
+  networking.networkmanager.enable = true;
 }
