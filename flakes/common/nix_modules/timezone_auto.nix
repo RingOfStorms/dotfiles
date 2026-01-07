9848701145f6a0b115fa87b215fe915007f6811b
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2026-01-06 22:17:59 -0600

    update auto timezone

--- flakes/common/nix_modules/timezone_auto.nix
+++ flakes/common/nix_modules/timezone_auto.nix
@@ -1,4 +1,17 @@
+{ lib, ... }:
 {
+  services.dbus.enable = lib.mkDefault true;
+
   time.timeZone = null;
   services.automatic-timezoned.enable = true;
+
+  systemd.services.automatic-timezoned = {
+    after = [ "dbus.socket" "systemd-timedated.service" ];
+    wants = [ "dbus.socket" "systemd-timedated.service" ];
+  };
+
+  systemd.services.automatic-timezoned-geoclue-agent = {
+    after = [ "dbus.socket" ];
+    wants = [ "dbus.socket" ];
+  };
 }
