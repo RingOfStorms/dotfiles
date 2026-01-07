00e25fa216c74c4b4285ff7d16131cb83c739bb6
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2026-01-06 22:53:24 -0600

    try again

--- flakes/common/nix_modules/timezone_auto.nix
+++ flakes/common/nix_modules/timezone_auto.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   services.dbus.enable = lib.mkDefault true;
   services.geoclue2.enable = true;
@@ -7,8 +7,13 @@
   services.automatic-timezoned.enable = true;
 
   systemd.services.automatic-timezoned = {
-    after = [ "dbus.socket" "systemd-timedated.service" ];
-    wants = [ "dbus.socket" "systemd-timedated.service" ];
+    after = [ "dbus.socket" "systemd-timedated.service" "geoclue.service" ];
+    wants = [ "dbus.socket" "systemd-timedated.service" "geoclue.service" ];
+    serviceConfig = {
+      ExecStartPre = "${lib.getExe' pkgs.coreutils "sleep"} 5";
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
   };
 
   systemd.services.automatic-timezoned-geoclue-agent = {
