7f59cebc129295ba78cc7ec15d37e0c12fd96844
RingOfStorms (Joshua Bell) <ringofstorms@gmail.com>
2026-01-09 17:43:02 -0600

    fix tmux config warn

--- flakes/common/hm_modules/tmux/default.nix
+++ flakes/common/hm_modules/tmux/default.nix
@@ -30,7 +30,7 @@
           };
         });
         extraConfig = ''
-          set -g @catppuccin_flavour 'mocha'
+          set -g @catppuccin_flavor 'mocha'
           set -g @catppuccin_window_left_separator ""
           set -g @catppuccin_window_right_separator " "
           set -g @catppuccin_window_middle_separator " █"
