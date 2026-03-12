47e85ace725068a471afba02357488a00b4e9fad
Joshua Bell <abc@joshuabell.xyz>
2026-03-12 16:24:13 -0500

    update sleep settings for new configs

--- flakes/common/nix_modules/no_sleep.nix
+++ flakes/common/nix_modules/no_sleep.nix
@@ -1,11 +1,23 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  useNewApi = lib.versionAtLeast config.system.stateVersion "26.05";
+in
 {
   # Turn off sleep
-  systemd.sleep.extraConfig = ''
-    [Sleep]
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowSuspendThenHibernate=no
-    AllowHybridSleep=no
-  '';
+  systemd.sleep = if useNewApi then {
+    settings.Sleep = {
+      AllowSuspend = "no";
+      AllowHibernation = "no";
+      AllowSuspendThenHibernate = "no";
+      AllowHybridSleep = "no";
+    };
+  } else {
+    extraConfig = ''
+      [Sleep]
+      AllowSuspend=no
+      AllowHibernation=no
+      AllowSuspendThenHibernate=no
+      AllowHybridSleep=no
+    '';
+  };
 }
