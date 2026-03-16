8b7a20d1374a559274974dd6744d8adf13d26e82
Joshua Bell <abc@joshuabell.xyz>
2026-03-16 10:35:47 -0500

    update secrets bao formats

--- flakes/secrets-bao/flake.nix
+++ flakes/secrets-bao/flake.nix
@@ -4,100 +4,75 @@
   inputs = { };
 
   outputs = { ... }:
+    let
+      substitute = secretPath: value:
+        if builtins.isAttrs value then
+          builtins.mapAttrs (_: v: substitute secretPath v) value
+        else if builtins.isList value then
+          map (v: substitute secretPath v) value
+        else if builtins.isString value then
+          builtins.replaceStrings [ "$SECRET_PATH" ] [ secretPath ] value
+        else
+          value;
+
+      deepMerge = a: b:
+        if builtins.isAttrs a && builtins.isAttrs b then
+          builtins.foldl'
+            (acc: key:
+              let
+                newVal = builtins.getAttr key b;
+                mergedVal =
+                  if builtins.hasAttr key acc then
+                    deepMerge (builtins.getAttr key acc) newVal
+                  else
+                    newVal;
+              in
+              acc // (builtins.listToAttrs [ { name = key; value = mergedVal; } ])
+            )
+            a
+            (builtins.attrNames b)
+        else if builtins.isList a && builtins.isList b then
+          a ++ b
+        else
+          b;
+
+      applyConfigChanges = secrets:
+        let
+          fragments = builtins.attrValues (builtins.mapAttrs (
+            name: s:
+            let
+              secretPath = s.path or ("/var/lib/openbao-secrets/" + name);
+            in
+            substitute secretPath (s.configChanges or { })
+          ) secrets);
+        in
+        builtins.foldl' deepMerge { } fragments;
+
+      applyHmChanges = secrets:
+        let
+          fragments = builtins.attrValues (builtins.mapAttrs (
+            name: s:
+            let
+              secretPath = s.path or ("/var/lib/openbao-secrets/" + name);
+            in
+            substitute secretPath (s.hmChanges or { })
+          ) secrets);
+
+          merged = builtins.foldl' deepMerge { } fragments;
+        in
+        if merged == { } then
+          { }
+        else
+          {
+            home-manager.sharedModules = [ (_: merged) ];
+          };
+    in
     {
       lib = {
-        applyConfigChanges = secrets:
-          let
-            substitute = secretPath: value:
-              if builtins.isAttrs value then
-                builtins.mapAttrs (_: v: substitute secretPath v) value
-              else if builtins.isList value then
-                map (v: substitute secretPath v) value
-              else if builtins.isString value then
-                builtins.replaceStrings [ "$SECRET_PATH" ] [ secretPath ] value
-              else
-                value;
+        inherit applyConfigChanges applyHmChanges;
 
-            deepMerge = a: b:
-              if builtins.isAttrs a && builtins.isAttrs b then
-                builtins.foldl'
-                  (acc: key:
-                    let
-                      newVal = builtins.getAttr key b;
-                      mergedVal =
-                        if builtins.hasAttr key acc then
-                          deepMerge (builtins.getAttr key acc) newVal
-                        else
-                          newVal;
-                    in
-                    acc // (builtins.listToAttrs [ { name = key; value = mergedVal; } ])
-                  )
-                  a
-                  (builtins.attrNames b)
-              else if builtins.isList a && builtins.isList b then
-                a ++ b
-              else
-                b;
-
-            fragments = builtins.attrValues (builtins.mapAttrs (
-              name: s:
-              let
-                secretPath = s.path or ("/var/lib/openbao-secrets/" + name);
-              in
-              substitute secretPath (s.configChanges or { })
-            ) secrets);
-          in
-          builtins.foldl' deepMerge { } fragments;
-
-        applyHmChanges = secrets:
-          let
-            substitute = secretPath: value:
-              if builtins.isAttrs value then
-                builtins.mapAttrs (_: v: substitute secretPath v) value
-              else if builtins.isList value then
-                map (v: substitute secretPath v) value
-              else if builtins.isString value then
-                builtins.replaceStrings [ "$SECRET_PATH" ] [ secretPath ] value
-              else
-                value;
-
-            deepMerge = a: b:
-              if builtins.isAttrs a && builtins.isAttrs b then
-                builtins.foldl'
-                  (acc: key:
-                    let
-                      newVal = builtins.getAttr key b;
-                      mergedVal =
-                        if builtins.hasAttr key acc then
-                          deepMerge (builtins.getAttr key acc) newVal
-                        else
-                          newVal;
-                    in
-                    acc // (builtins.listToAttrs [ { name = key; value = mergedVal; } ])
-                  )
-                  a
-                  (builtins.attrNames b)
-              else if builtins.isList a && builtins.isList b then
-                a ++ b
-              else
-                b;
-
-            fragments = builtins.attrValues (builtins.mapAttrs (
-              name: s:
-              let
-                secretPath = s.path or ("/var/lib/openbao-secrets/" + name);
-              in
-              substitute secretPath (s.hmChanges or { })
-            ) secrets);
-
-            merged = builtins.foldl' deepMerge { } fragments;
-          in
-          if merged == { } then
-            { }
-          else
-            {
-              home-manager.sharedModules = [ (_: merged) ];
-            };
+        applyChanges = secrets:
+          deepMerge (applyConfigChanges secrets) (applyHmChanges secrets);
       };
 
       nixosModules = {
