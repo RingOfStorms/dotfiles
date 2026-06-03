00f456cb41837263e629d0b45c398173122c6af0
Joshua Bell <abc@joshuabell.xyz>
2026-06-03 00:07:38 -0500

    cleanup stale comments

--- flakes/common/hm_modules/atuin.nix
+++ flakes/common/hm_modules/atuin.nix
@@ -1,9 +1,11 @@
 { ... }:
-# TODO setup auto secret/login for sync
+# Auto secret/login for sync is handled system-side by the
+# `ringofstorms.atuin` module (flakes/common/nix_modules/atuin.nix),
+# which logs the user into the sync server on boot from a secrets file.
 {
   programs.atuin = {
     enable = true;
-    enableZshIntegration = true; # TODO make dynamic?
+    enableZshIntegration = true;
     flags = [ "--disable-up-arrow" ];
     settings = {
       workspaces = true;
