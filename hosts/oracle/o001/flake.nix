69e1d7f36bbe0dcaeacbb081f1ac2b91f48dff55
Joshua Bell <abc@joshuabell.xyz>
2026-06-02 21:28:22 -0500

    update 26.06 - o001

--- hosts/oracle/o001/flake.nix
+++ hosts/oracle/o001/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:rycee/home-manager/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager.url = "github:rycee/home-manager/release-26.05";
     ros_neovim.url = "git+https://git.joshuabell.xyz/ringofstorms/nvim";
 
     # Use relative to get current version for testing
