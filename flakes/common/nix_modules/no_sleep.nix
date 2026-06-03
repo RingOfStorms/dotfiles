21e6d474d10f21b185919d32e249d074edb7d52f
Joshua Bell <abc@joshuabell.xyz>
2026-06-02 20:57:55 -0500

    fix no sleep

--- flakes/common/nix_modules/no_sleep.nix
+++ flakes/common/nix_modules/no_sleep.nix
@@ -1,6 +1,9 @@
 { config, lib, ... }:
 let
-  useNewApi = lib.versionAtLeast config.system.stateVersion "26.05";
+  # The `systemd.sleep.extraConfig` option was removed in NixOS 26.05 in favor
+  # of `systemd.sleep.settings`. This is tied to the nixpkgs release, NOT the
+  # host's stateVersion (which we intentionally don't bump on existing hosts).
+  useNewApi = lib.versionAtLeast config.system.nixos.release "26.05";
 in
 {
   # Turn off sleep
