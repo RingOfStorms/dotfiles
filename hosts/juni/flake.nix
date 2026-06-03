2c8c8e64224e3fd14e8d85baf633a9cfe641d2ab
Joshua Bell <abc@joshuabell.xyz>
2026-06-02 21:04:46 -0500

    update 26.06 - juni

--- hosts/juni/flake.nix
+++ hosts/juni/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:rycee/home-manager/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager.url = "github:rycee/home-manager/release-26.05";
 
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
